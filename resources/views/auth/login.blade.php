6ab13f023bd90845007441acfa4ee6d887a17cff
hajidalakhtar <hajidalakhtar@gmail.com>
2025-01-13 18:09:38 +0700

    .

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -146,9 +146,7 @@
                 </form>
             </div>
         </div>
-        <div class="text-center text-secondary mt-3">
-            Don't have account yet? <a href="./sign-up.html" tabindex="-1">Sign up</a>
-        </div>
+
     </div>
 </div>
 <!-- Libs JS -->
